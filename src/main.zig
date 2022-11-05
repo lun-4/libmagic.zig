421022dcefb4d1c8171748e89e0fc6c19ef367ce
Luna <git@l4.pm>
2022-11-05 00:06:08 -0300

    fallback to static magic when system fails

--- src/main.zig
+++ src/main.zig
@@ -5,124 +5,133 @@ pub const c = @cImport({
     @cInclude("magic.h");
 });
 
-const magic_file_data = @embedFile("magic.mgc");
-
 const logger = std.log.scoped(.libmagic);
+
+// Claler owns returned memory.
+fn loadStaticMagic(allocator: std.mem.Allocator) ![:0]const u8 {
+    const datadir = try std.fs.getAppDataDir(allocator, "libmagic");
+    defer allocator.free(datadir);
+    logger.warn("data dir {s}", .{datadir});
+
+    _ = std.fs.openDirAbsolute(datadir, .{}) catch |err| switch (err) {
+        error.FileNotFound => {
+            try std.fs.makeDirAbsolute(datadir);
+        },
+        else => return err,
+    };
+
+    const database_file_path = try std.fs.path.join(allocator, &[_][]const u8{ datadir, "magic.mgc" });
+    defer allocator.free(database_file_path);
+
+    logger.warn("dumping static into {s}", .{database_file_path});
+
+    const database_file = try std.fs.createFileAbsolute(database_file_path, .{ .truncate = true });
+    defer database_file.close();
+
+    // lmao who here 7mb binary
+    const magic_file_data = @embedFile("magic.mgc");
+    _ = try database_file.write(magic_file_data);
+
+    const database_file_cstr = try std.cstr.addNullByte(allocator, database_file_path);
+    return database_file_cstr;
+}
+
+const POSSIBLE_MAGICDB_PREFIXES = [_][:0]const u8{
+    "/usr/share/misc/magic",
+    "/usr/share/misc",
+    "/usr/local/share/misc",
+    "/etc",
+};
+
+fn findSystemMagicFile(allocator: std.mem.Allocator) !?[:0]const u8 {
+    var found_prefix: ?usize = null;
+
+    for (POSSIBLE_MAGICDB_PREFIXES) |prefix, prefix_index| {
+        var dir = std.fs.cwd().openDir(prefix, .{}) catch |err| switch (err) {
+            error.FileNotFound, error.NotDir => continue,
+            else => return err,
+        };
+        defer dir.close();
+
+        var magic_file = dir.openFile("magic.mgc", .{}) catch |err| switch (err) {
+            error.FileNotFound => continue,
+            else => return err,
+        };
+        defer magic_file.close();
+
+        // we have a magic_file
+        found_prefix = prefix_index;
+        break;
+    }
+
+    const magicdb_prefix = POSSIBLE_MAGICDB_PREFIXES[
+        found_prefix orelse return null
+    ];
+
+    const magicdb_path = try std.fmt.allocPrint(allocator, "{s}/magic", .{magicdb_prefix});
+    defer allocator.free(magicdb_path);
+
+    const path_cstr = try std.cstr.addNullByte(allocator, magicdb_path);
+    return path_cstr;
+}
 
 const MimeCookie = struct {
     cookie: c.magic_t,
 
     const Self = @This();
 
-    const POSSIBLE_MAGICDB_PREFIXES = [_][:0]const u8{
-        "/usr/share/misc",
-        "/usr/local/share/misc",
-        "/etc",
+    const LoadingMode = enum {
+        // TODO system_only,
+        // TODO static_only,
+        fallback_to_static,
     };
 
     const InitOptions = struct {
-        static_magic_file: bool = true,
+        loading_mode: LoadingMode = .fallback_to_static,
     };
 
     pub fn init(allocator: std.mem.Allocator, options: InitOptions) !Self {
+        _ = options;
+
         var cookie = c.magic_open(
             c.MAGIC_MIME_TYPE | c.MAGIC_CHECK | c.MAGIC_SYMLINK | c.MAGIC_ERROR,
         ) orelse return error.MagicCookieFail;
 
-        if (options.static_magic_file) {
-            const datadir = try std.fs.getAppDataDir(allocator, "libmagic");
-            defer allocator.free(datadir);
-            logger.warn("data dir {s}", .{datadir});
+        const maybe_system_magic = try findSystemMagicFile(allocator);
+        defer if (maybe_system_magic) |system_magic| allocator.free(system_magic);
 
-            _ = std.fs.openDirAbsolute(datadir, .{}) catch |err| switch (err) {
-                error.FileNotFound => {
-                    try std.fs.makeDirAbsolute(datadir);
-                },
-                else => return err,
-            };
+        const bundled_magic = try loadStaticMagic(allocator);
+        defer allocator.free(bundled_magic);
 
-            const database_file_path = try std.fs.path.join(allocator, &[_][]const u8{ datadir, "magic.mgc" });
-            defer allocator.free(database_file_path);
+        // try system magic first,
+        // if that fails, fallback to static magic (should always work);
 
-            logger.warn("dumping static into {s}", .{database_file_path});
+        var path: []const u8 = undefined;
 
-            const database_file = try std.fs.createFileAbsolute(database_file_path, .{ .truncate = true });
-            defer database_file.close();
-            _ = try database_file.write(magic_file_data);
-
-            const database_file_cstr = try std.cstr.addNullByte(allocator, database_file_path);
-            defer allocator.free(database_file_cstr);
-
-            if (c.magic_load(cookie, database_file_cstr.ptr) == -1) {
+        if (maybe_system_magic) |system_magic| {
+            logger.warn("loading magic file {s}", .{system_magic});
+            if (c.magic_load(cookie, system_magic.ptr) == -1) {
                 const magic_error_value = c.magic_error(cookie);
-                logger.err("failed to load magic buffer: {s}", .{magic_error_value});
-                return error.MagicFileFail;
-            }
+                logger.warn("failed to load magic file from system: {s}", .{magic_error_value});
 
-            if (c.magic_check(cookie, database_file_cstr.ptr) == -1) {
-                const magic_error_value = c.magic_error(cookie);
-                logger.err("failed to check magic file: {s}", .{magic_error_value});
-                return error.MagicFileFail;
-            }
-        } else {
-            // this attempts to find the path for the magic db file dynamically
-            // through some paths i have found around the systems i have.
-            //
-            // libmagic's build process enables you to override the default
-            // path to the magic file, which means that doing a static build of it
-            // means it won't work on a separate system since it doesn't have
-            // that one hardcoded in.
-            //
-            // a future iteration might bundle the magic database with the
-            // executable through possibly, @embedFile, then dump that into a
-            // temporary file for super compatibility with windows and macos
+                // fallback to static here
 
-            var found_prefix: ?usize = null;
-
-            for (POSSIBLE_MAGICDB_PREFIXES) |prefix, prefix_index| {
-                var dir = std.fs.cwd().openDir(prefix, .{}) catch |err| switch (err) {
-                    error.FileNotFound, error.NotDir => continue,
-                    else => return err,
-                };
-                defer dir.close();
-
-                var magic_file = dir.openFile("magic.mgc", .{}) catch |err| switch (err) {
-                    error.FileNotFound => continue,
-                    else => return err,
-                };
-                defer magic_file.close();
-
-                // we have a magic_file
-                found_prefix = prefix_index;
-                break;
-            }
-
-            const magicdb_prefix = POSSIBLE_MAGICDB_PREFIXES[
-                found_prefix orelse {
-                    logger.err("failed to locate magic file", .{});
-                    return error.MagicNotFound;
+                if (c.magic_load(cookie, bundled_magic.ptr) == -1) {
+                    const magic_error_value_bundle = c.magic_error(cookie);
+                    logger.err("failed to load magic file from bundle: {s}", .{magic_error_value_bundle});
+                    return error.MagicFileLoadFail;
+                } else {
+                    path = bundled_magic;
                 }
-            ];
-
-            const magicdb_path = try std.fmt.allocPrint(allocator, "{s}/magic", .{magicdb_prefix});
-            defer allocator.free(magicdb_path);
-
-            const path_cstr = try std.cstr.addNullByte(allocator, magicdb_path);
-            defer allocator.free(path_cstr);
-
-            logger.info("loading magic file at prefix {s}", .{path_cstr});
-
-            if (c.magic_load(cookie, path_cstr.ptr) == -1) {
-                const magic_error_value = c.magic_error(cookie);
-                logger.err("failed to load magic file: {s}", .{magic_error_value});
-                return error.MagicFileFail;
+            } else {
+                path = system_magic;
             }
+        }
 
-            if (c.magic_check(cookie, path_cstr.ptr) == -1) {
-                const magic_error_value = c.magic_error(cookie);
-                logger.err("failed to check magic file: {s}", .{magic_error_value});
-                return error.MagicFileFail;
-            }
+        if (c.magic_check(cookie, path.ptr) == -1) {
+            const magic_error_value = c.magic_error(cookie);
+            logger.err("failed to check magic file: {s}", .{magic_error_value});
+            return error.MagicFileCheckFail;
         }
 
         return MimeCookie{ .cookie = cookie };
